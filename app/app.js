219ecc24da47872c7221ee69025420dd54345b92
TommyCakes <tomhwarhurst@aol.com>
2016-01-28 21:28:29 +0000

    Added wed / music and film sections, created nav

--- app/app.js
+++ app/app.js
@@ -1,1 +1,19 @@
 angular.module('App', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+      .state('home', {
+        url:"/",
+        templateUrl: 'app/states/home.html'
+      })
+      .state('music', {
+        url:"/music",
+        templateUrl: 'app/states/music.html'
+      })
+      .state('film', {
+        url:"/film",
+        templateUrl: 'app/states/film.html'
+      })
+  });
